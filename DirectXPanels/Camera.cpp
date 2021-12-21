a74171b545ea6594ec2c4c2e95ef1b1243f15463
David Stahl <david.stahl97@outlook.com>
2021-12-21 20:17:05 +0100

    remove old camera code

--- DirectXPanels/Camera.cpp
+++ DirectXPanels/Camera.cpp
@@ -5,15 +5,10 @@ using namespace DirectX;
 
 void Camera::Update() 
 {
-	//static const XMVECTORF32 eye = { 0.0f, 1.7f, 0.1f, 0.0f };
 	static const XMVECTORF32 at = { 0.0f, 0.0f, 0.0f, 0.0f };
 	static const XMVECTORF32 up = { 0.0f, 1.0f, 0.0f, 0.0f };
 
-	auto& phiVector = XMVectorSet(XMScalarSin(m_Phi), 0, XMScalarCos(m_Phi), 0);
-	auto& epsilonVector = XMVectorSet(0, XMScalarSin(m_Epsilon), XMScalarCos(m_Epsilon), 0);
-
 	auto& zero = XMVectorSet(0, 0, 1, 0);
-	auto& epsilonDifference = epsilonVector - zero;
 
 	auto& phiRotationVector = XMVector4Transform(zero, XMMatrixRotationY(m_Phi));
 	auto& eye = XMVector4Transform(phiRotationVector,
